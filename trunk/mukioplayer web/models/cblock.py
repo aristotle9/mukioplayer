08b348d1f6e19674ad4bc27ecbdbca27b7b67cf9
mukioplay <mukioplay@81c0934e-2037-d0d5-095e-d9327ae16866>
2010-09-27 09:04:11 +0000

    删掉重来

--- trunk/mukioplayer web/models/cblock.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import db
-from video import Video
-
-class CBlock(db.Model):
-  data = db.TextProperty()
-  link = db.StringProperty()
-  postdate = db.DateTimeProperty(auto_now_add=True)
-  cid = db.ReferenceProperty(Video)
-
