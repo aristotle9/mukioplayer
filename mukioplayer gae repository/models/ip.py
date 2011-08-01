0f412d0b4000bc4ac405a6b15813f99d1974d710
mukioplay <mukioplay@81c0934e-2037-d0d5-095e-d9327ae16866>
2011-08-01 05:10:47 +0000

    原始副本保存

--- mukioplayer gae repository/models/ip.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import db
-
-class Ip(db.Model):
-  ip = db.StringProperty()
-  lastpostdate = db.DateTimeProperty()
--- mukioplayer gae repository/models/video.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import db
-
-class Video(db.Model):
-  typ = db.StringProperty()
-  vid = db.StringProperty()
